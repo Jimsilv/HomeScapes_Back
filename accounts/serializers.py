2f9375a060449ad91e76fbff2b7bb7c05912686d
Jimver_Wayne Silvestre <jasilvestre@student.hau.edu.ph>
2025-03-06 18:21:04 +0800

    Login and Register

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,18 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password','first_name', 'last_name')
         extra_kwargs = {
             'email': {'required': True},
-            'password': {'write_only': True},
-            'username': {'write_only': True}
+            'password': {'required': True},
+            'username': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
         }
 
     def create(self,validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name']
         )
 
         return user
