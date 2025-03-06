7d307547dc5303fbcbc2addca779ae77c17baa6d
Jimver_Wayne Silvestre <jasilvestre@student.hau.edu.ph>
2025-03-06 12:59:07 +0800

    Login and Register

---
+++ accounts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {
+            'email': {'required': True},
+            'password': {'write_only': True},
+            'username': {'write_only': True}
+        }
+
+    def create(self,validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
+        return user
+
+
